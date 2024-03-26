bbfd913e233b82cdcb51782b051bb043cc83c07c
romainsessa <romainsessa@gmail.com>
2024-03-26 15:02:56 +0100

    minimal javafx app

--- src/main/java/fr/iris/maven/common/Bootstrap.java
+++ src/main/java/fr/iris/maven/common/Bootstrap.java
@@ -1,43 +1,19 @@
 package fr.iris.maven.common;
 
-import fr.iris.maven.character.Cheater;
-import fr.iris.maven.character.Fighter;
-import fr.iris.maven.character.GameCharacter;
-import fr.iris.maven.character.Healer;
-import fr.iris.maven.character.IFighter;
-import fr.iris.maven.character.IHealer;
-import fr.iris.maven.exception.InvalidCharacterException;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Bootstrap {
+public class Bootstrap extends Application  {
 	
 	
 	public static void main(String[] args) {
-		
-		GameCharacter fighter1;
-		
-		try {
-			fighter1 = new Fighter("superman", 100, 100);
-			System.out.println(fighter1);
-			
-			IHealer healer1 = new Healer("test", 100, 1000);
-			healer1.heal(fighter1);		
-			((Healer) healer1).autoHeal();
-			
-			System.out.println(healer1);		
-			System.out.println(fighter1);
-			
-			Cheater cheater1 = new Cheater("1");
-			cheater1.attack(fighter1);
-			
-			System.out.println(cheater1);		
-			System.out.println(fighter1);
-			
-		} catch (InvalidCharacterException e) {
-			System.out.println("Log : les informations saisies sont erronées");
-		}
-		
-		
-		
+		launch(args);
 	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.show();
+	}
+
 
 }
